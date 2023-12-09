70fff35ebebc0a7607c3d96c4ab8d135e53d2c39
arbems <arbems@outlook.com>
2023-12-09 21:23:00 +0100

    Initial create

---
+++ src/8.x/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppWithMudBlazor
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
